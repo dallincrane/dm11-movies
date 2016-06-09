0933b4f60c7109cfa530e86bbcb8a93fe47df4eb
Dallin Crane <dallincrane@gmail.com>
2016-06-09 12:50:44 -0600

    initial

---
+++ models/movies.js
@@ -0,0 +1,6 @@
+module.exports = [
+  { title: 'Con Air', ridiculous_rating: 11 },
+  { title: 'Ghost Rider', ridiculous_rating: 6 },
+  { title: 'Raising Arizona', ridiculous_rating: 2 },
+  { title: 'Sorcerers Apprentice', ridiculous_rating: 98 }
+]
