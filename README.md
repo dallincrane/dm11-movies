79ad9be6ecf7e74bb6e04b1bcd2cc304e81e2543
Dallin Crane <dallincrane@gmail.com>
2016-06-10 12:33:45 -0600

    add cart sessions and enpoints

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-If you clone this make sure to do the following!  
-From the project folder in your terminal run:  
+### If you clone this make sure to do the following!  
+
+* From the project folder in your terminal run:  
 `npm install`
+
+* Add a `/config.json` file with:  
+`{ "sessionSecret": "mistbornfordummies" }`
--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var session = require('express-session')
+
+var config = require('./config.json')
 
 var moviesController = require('./controllers/movies_controller')
+var cartController = require('./controllers/cart_controller')
 
 var app = express()
 
 app.use(bodyParser.json())
 app.use(express.static('public'))
+app.use(session({
+  secret: config.sessionSecret,
+  saveUninitialized: false,
+  resave: false
+}))
 
 app.use(function(req, res, next) {
   console.log('body: ', req.body)
@@ -18,6 +27,9 @@ app.get('/movies/:id', moviesController.show)
 app.put('/movies/:id', moviesController.update)
 app.post('/movies', moviesController.create)
 app.delete('/movies/:id', moviesController.destroy)
+
+app.get('/cart', cartController.index)
+app.post('/cart', cartController.create)
 
 var port = 3000
 app.listen(port, function() {
